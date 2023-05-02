4a56acc36cdb34516832bd9cbbeed6774d030186
cyber-tatarin <dmitriyseur@gmail.com>
2023-05-02 15:13:06 +0300

    db models fix

--- db/models.py
+++ db/models.py
@@ -22,21 +22,21 @@ class Base(DeclarativeBase):
 class Task(Base):
     __tablename__ = "tasks"
     
-    client_tg_id = Column("current_tg_id", Integer, primary_key=True)
+    client_tg_id = Column("slient_tg_id", String(10), primary_key=True)
     current_task = Column("current_task", Integer)
     
 
 class State(Base):
     __tablename__ = "states"
     
-    client_tg_id = Column("client_tg_id", Integer, primary_key=True)
-    current_state = Column("current_state", String)
+    client_tg_id = Column("client_tg_id", String(10), primary_key=True)
+    current_state = Column("current_state", String(50))
 
 
 class User(Base):
     __tablename__ = "users"
 
-    client_tg_id = Column("current_tg_id", Integer, primary_key=True)
-    order_id = Column("order_id", String, unique=True)
+    client_tg_id = Column("client_tg_id", String(10), primary_key=True)
+    order_id = Column("order_id", String(30), unique=True)
     have_paid = Column("have_paid", Boolean, default=False)
     
