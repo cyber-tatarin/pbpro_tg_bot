5614a8d3ec8d83ddda9a9df125955752534484f3
cyber-tatarin <dmitriyseur@gmail.com>
2023-05-02 21:50:57 +0300

    aiohttp config update

--- root/successful_payment_webhook/main.py
+++ root/successful_payment_webhook/main.py
@@ -28,6 +28,6 @@ app = web.Application()
 app.add_routes([web.post('/payment', handle_post_request)])
 
 if __name__ == '__main__':
-    web.run_app(app, host='0.0.0.0', port=8000)
+    web.run_app(app, host='0.0.0.0', port=5000)
 
     
