b40aa1dbb9cfadbd0b122353c03333a3dddee97b
cyber-tatarin <dmitriyseur@gmail.com>
2023-05-04 22:50:29 +0300

    fix states backup to db

--- root/tg/main.py
+++ root/tg/main.py
@@ -297,26 +297,25 @@ async def payment_confirmed(user_id):
 
 
 async def backup_user_states():
-    async with storage:
-        async with db.Session as session:
-            for user_id, state_dict in await storage.get_data():
-                task = models.Task(client_tg_id=user_id, current_state=state_dict['state'])
-                await session.add(task)
-            await session.commit()
-            if session.is_active:
-                await session.close()
+
+    async with db.Session as session:
+        for user_id, state_dict in await storage.get_data():
+            task = models.Task(client_tg_id=user_id, current_state=state_dict['state'])
+            await session.add(task)
+        await session.commit()
+        if session.is_active:
+            await session.close()
 
 
 # Define a function to restore the user states from the database
 async def restore_user_states():
-    async with storage:
-        async with db.Session as session:
-            for row in session.query(models.State).all():
-                await storage.set_state(user=row.client_tg_id, state=row.current_state)
-            await session.query(models.State).delete()
-            await session.commit()
-            if session.is_active:
-                await session.close()
+    async with db.Session as session:
+        for row in session.query(models.State).all():
+            await storage.set_state(user=row.client_tg_id, state=row.current_state)
+        await session.query(models.State).delete()
+        await session.commit()
+        if session.is_active:
+            await session.close()
 
 
 async def on_startup(_):
