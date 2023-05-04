44f127bfdfa241ee5b9d9db6b1a7a224d8abc526
cyber-tatarin <dmitriyseur@gmail.com>
2023-05-04 17:09:39 +0300

    logger update

--- root/prodamus/main.py
+++ root/prodamus/main.py
@@ -13,7 +13,7 @@ logger = get_logger()
 async def confirm_payment(signature, data):
     # if verify_signature(signature, data):
     if float(data['sum']) >= 2000 and data['payment_status'] == 'success':
-        logger.info('prodamus/confirm_payment')
+        logger.info(f'prodamus/confirm_payment {data["order_num"]}')
         session = db.Session()
         user = session.query(models.User).filter(models.User.order_id == data['order_num']).first()
         if user:
@@ -24,6 +24,7 @@ async def confirm_payment(signature, data):
                 session.close()
             await payment_confirmed(user.client_tg_id)
             return
+        logger.error(f'user for order_id: {data["order_num"]} is not found')
         return
     return
 
